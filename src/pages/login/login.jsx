4df1f8a20776d0953e053515b951c049e61f8152
rust <zkshangxiao@126.com>
2019-09-02 21:06:08 +0800

    011 Login组件基本布局

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -4,10 +4,22 @@
 
 import React, { Component } from "react"
 
+import './login.less'
+import logo from './images/logo.png'
+
 export default class Login extends Component {
     render() {
         return (
-            <div>Login 登录</div>
+            <div className="login">
+                <header className="login-header">
+                    <img src={logo} alt="logo"/>
+                    <h1>领航内容标注审核系统</h1>
+                </header>
+                <section className="login-content">
+                    <h2>用户登录</h2>
+                    <div>Form组件标签</div>
+                </section>
+            </div>
         )
     }
 }
