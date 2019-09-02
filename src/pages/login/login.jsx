abff8c87621d51b330397dfbf3fb2a76821621a1
rust <zkshangxiao@126.com>
2019-09-03 07:11:41 +0800

    015 Login组件 Form声明式验证

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -8,13 +8,24 @@ import { Form, Icon, Input, Button } from 'antd';
 import './login.less'
 import logo from './images/logo.png'
 
-export default class Login extends Component {
+class Login extends Component {
 
     handleSubmit = (event) => {
-
+    // 阻止事件的默认行为
+        event.preventDefault()
+    // 得到 form 对象
+        const form = this.props.form
+    // 获取表单项的输入数据
+        const values = form.getFieldsValue()
+        console.log('handleSubmit() 的值', values)
     }
 
+
     render() {
+
+        const form = this.props.form
+        const { getFieldDecorator } = this.props.form
+
         return (
             <div className="login">
                 <header className="login-header">
@@ -25,17 +36,34 @@ export default class Login extends Component {
                     <h2>用户登录</h2>
                     <Form onSubmit={this.handleSubmit} className="login-form">
                         <Form.Item>
+                        {
+                            getFieldDecorator('username', {
+                            //  配置对象: 属性名是特定的一些名称
+                            //  声明式验证: 直接使用定义好的验证规则进行验证
+                                rules: [
+                                    { required: true, whitespace: true, message: '用户名不能为空' },
+                                    { min: 4, message: '用户名不少于4位' },
+                                    { max: 12, message: '用户名最多12位' },
+                                    { pattern: /^[a-zA-Z0-9_]+$/, message: '用户名必须由英文、数字或下划线组成' },
+                                ],
+                            })(
                             <Input
                                 prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
                                 placeholder="输入用户名"
-                            />
+                            />,
+                        )}
                         </Form.Item>
                         <Form.Item>
+                        {
+                            getFieldDecorator('password', {})(
                             <Input
                                 prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
                                 type="password"
                                 placeholder="输入密码"
                             />
+                            )
+                        }
+
                         </Form.Item>
                         <Form.Item>
                             <Button type="primary" htmlType="submit" className="login-form-button">
@@ -48,3 +76,10 @@ export default class Login extends Component {
         )
     }
 }
+
+/*
+* 包装 Form 组件生成一个新的组件：Form(Login)
+* 新组建会向 Form 组件传递一个强大的对象属性：form
+* */
+const WrapLogin  = Form.create()(Login)
+export default WrapLogin
