4e69a54457ce6cd0e4f8133e02df6e8d7c096ee3
rust <zkshangxiao@126.com>
2019-09-04 19:27:52 +0800

    032 动态显示菜单列表 map + 递归调用

--- src/components/left-nav/index.jsx
+++ src/components/left-nav/index.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import { Menu, Icon, Button } from 'antd';
 
 import logo from '../../assets/images/logo.png'
+import menuList from "../../config/menuConfig";
 import './index.less'
 
 const { SubMenu } = Menu;
@@ -12,6 +13,71 @@ const { SubMenu } = Menu;
 */
 
 export default class LeftNav extends Component {
+
+    /*
+        根据 menu 的数据数组生成对应的标签数组
+        使用 map 加 递归调用
+     */
+    getMenuNodes = (menuList) => {
+        return menuList.map(item => {
+            /* 
+                {
+                    title: '首页', // 菜单标题名称
+                    key: '/home', // 对应的path
+                    icon: 'home', // 图标名称
+                    children: [], // 可能有，可能没有
+                }
+
+                // 可能是这个形式
+                <Menu.Item key="/home">
+                    <Link to='/home'>
+                        <Icon type="pie-chart" />
+                        <span>首 页</span>
+                    </Link>
+                </Menu.Item>
+
+                // 也可能是这个形式
+                <SubMenu
+                key="sub1"
+                title={
+                    <span>
+                        <Icon type="shop" />
+                        <span>商 品</span>
+                    </span>
+                  }
+                >
+                </Menu.Item>
+                </SubMenu>
+
+            */
+            if (!item.children) {
+                return (
+                    <Menu.Item key={item.key}>
+                        <Link to={item.key}>
+                            <Icon type={item.icon} />
+                            <span>{item.title}</span>
+                        </Link>
+                    </Menu.Item>
+                )
+            } else {
+                return (
+                    <SubMenu
+                        key={item.key}
+                        title={
+                            <span>
+                                <Icon type={item.icon} />
+                                <span>{item.title}</span>
+                            </span>
+                        }
+                    >
+                        {this.getMenuNodes(item.children)}
+                    </SubMenu >
+                )
+            }
+
+        })
+    }
+
     render() {
         return (
             <div className="left-nav">
@@ -24,49 +90,54 @@ export default class LeftNav extends Component {
                     mode="inline"
                     theme="dark"
                 >
-                    <Menu.Item key="/home">
-                        <Link to='/home'>
-                            <Icon type="pie-chart" />
-                            <span>首 页</span>
+                    {/* 
+                <Menu.Item key="/home">
+                    <Link to='/home'>
+                        <Icon type="pie-chart" />
+                        <span>首 页</span>
+                    </Link>
+                </Menu.Item>
+                <SubMenu
+                    key="sub1"
+                    title={
+                        <span>
+                            <Icon type="shop" />
+                            <span>商 品</span>
+                        </span>
+                    }
+                >
+                    <Menu.Item key="/category">
+                        <Link to='/category'>
+                            <Icon type="shop" />
+                            <span>品类管理</span>
                         </Link>
                     </Menu.Item>
-                    <SubMenu
-                        key="sub1"
-                        title={
-                            <span>
-                                <Icon type="shop" />
-                                <span>商 品</span>
-                            </span>
-                        }
-                    >
-                        <Menu.Item key="/category">
-                            <Link to='/category'>
-                                <Icon type="shop" />
-                                <span>品类管理</span>
-                            </Link>
-                        </Menu.Item>
 
-                        <Menu.Item key="/product">
-                            <Link to='/product'>
-                                <Icon type="shop" />
-                                <span>商品管理</span>
-                            </Link>
-                        </Menu.Item>
+                    <Menu.Item key="/product">
+                        <Link to='/product'>
+                            <Icon type="shop" />
+                            <span>商品管理</span>
+                        </Link>
+                    </Menu.Item>
 
-                        <Menu.Item key="/user">
-                            <Link to='/user'>
-                                <Icon type="user" />
-                                <span>用户管理</span>
-                            </Link>
-                        </Menu.Item>
+                    <Menu.Item key="/user">
+                        <Link to='/user'>
+                            <Icon type="user" />
+                            <span>用户管理</span>
+                        </Link>
+                    </Menu.Item>
 
-                        <Menu.Item key="/role">
-                            <Link to='/role'>
-                                <Icon type="idcard" />
-                                <span>角色管理</span>
-                            </Link>
-                        </Menu.Item>
-                    </SubMenu>
+                    <Menu.Item key="/role">
+                        <Link to='/role'>
+                            <Icon type="idcard" />
+                            <span>角色管理</span>
+                        </Link>
+                    </Menu.Item>
+                </SubMenu> */}
+
+                    {
+                        this.getMenuNodes(menuList)
+                    }
                 </Menu>
             </div>
         )
