68ac55ee76911be2c4e04dfc2649bbcbc57abc4e
rust <zkshangxiao@126.com>
2019-09-04 09:10:06 +0800

    028 Admin组件 搭建整体界面结构

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,7 +1,13 @@
 import React, {Component} from 'react'
 import {Redirect} from 'react-router-dom'
+import { Layout } from 'antd'
 
 import memoryUtils from '../../utils/memoryUtils'
+
+import LeftNav from '../../components/left-nav'
+import Header from '../../components/header'
+
+const { Footer, Sider, Content } = Layout;
 
 /*
 后台管理的路由组件
@@ -15,9 +21,16 @@ export default class Admin extends Component {
       return <Redirect to='/login'/>
     }
     return (
-      <div>
-        欢迎 {user.username} 来到后台管理
-      </div>
+      <Layout style={{ height: '100%'}}>
+        <Sider>
+          <LeftNav />
+        </Sider>
+          <Layout>
+            <Header>Header</Header>
+            <Content style={{  backgroundColor: 'antiquewhite' }}>Content</Content>
+            <Footer style={{ textAlign: 'center', color: '#999999' }}>腾讯开放平台 QQ物联 DNSPod 微信公众平台 腾讯优图 腾讯蓝鲸 企业QQ 腾讯微云 腾讯文档 友情链接</Footer>
+          </Layout>
+      </Layout> 
     )
   }
 }
